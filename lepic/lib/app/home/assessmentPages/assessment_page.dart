33d35a88dfd86241a243fd332b288b7e940c6eaf
ayberk bozkus <ayberkbozkus@gmail.com>
2022-01-09 18:32:40 +0300

    speech2text is added
    Run code : flutter run --no-sound-null-safety

--- lepic/lib/app/home/assessmentPages/assessment_page.dart
+++ lepic/lib/app/home/assessmentPages/assessment_page.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:exp/app/home/assessmentPages/create_assessment_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:exp/app/home/assessmentPages/create_assessment_page.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 
 class AssessmentPage extends StatefulWidget {
@@ -24,6 +27,17 @@ class AssessmentPage extends StatefulWidget {
 
 class _AssessmentPageState extends State<AssessmentPage> {
 
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press the button and start speaking';
+  double _confidence = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +53,42 @@ class _AssessmentPageState extends State<AssessmentPage> {
         elevation: 1,
         actions: <Widget>[
           FloatingActionButton(
-              onPressed: () => CreateAssessmentPage.show(context),
+              onPressed: () {},
               heroTag: 'add',
               child: Icon(Icons.add)),
         ],
       ),
-      body: _buildContents(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                          _text,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.blue[300],
+                          ),
+                        ),
+            ])
+          ),
+        ),
+      
     );
   }
 
@@ -77,5 +121,28 @@ class _AssessmentPageState extends State<AssessmentPage> {
 
   }
 
-
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
 }
+
+
